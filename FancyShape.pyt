11554d25f9b211f00c3edecd5ec9673b626b1a23
andygarfield <andyjgarfield@gmail.com>
2015-11-19 17:57:53 -0500

    Added Line to Polygon conversion.

--- FancyShape.pyt
+++ FancyShape.pyt
@@ -1,5 +1,5 @@
 import arcpy
-from conversion import polygon_2_line
+import conversion
 
 
 class Toolbox(object):
@@ -10,14 +10,14 @@ class Toolbox(object):
         self.alias = "fancyshape"
 
         # List of tool classes associated with this toolbox
-        self.tools = [Polygon2Line]
+        self.tools = [Polygon2Line, Line2Polygon]
 
 
 class Polygon2Line(object):
     def __init__(self):
         """Define the tool (tool name is the name of the class)."""
         self.label = "Polygon to Line"
-        self.description = "Converts a polygon feature class into a line feature class."
+        self.description = "Converts a polygon feature class into a polyline feature class."
         self.canRunInBackground = False
 
     def getParameterInfo(self):
@@ -57,4 +57,50 @@ class Polygon2Line(object):
         in_features = parameters[0].valueAsText
         out_features = parameters[1].valueAsText
 
-        polygon_2_line(in_features, out_features)
+        conversion.polygon_2_line(in_features, out_features)
+
+class Line2Polygon(object):
+    def __init__(self):
+        """Define the tool (tool name is the name of the class)."""
+        self.label = "Line to Polygon"
+        self.description = "Converts a polyline feature class into a polygon feature class."
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        in_features = arcpy.Parameter(
+            displayName='Input Features',
+            name='in_features',
+            datatype='GPFeatureLayer',
+            parameterType='Required',
+            direction='Input'
+        )
+        out_features = arcpy.Parameter(
+            displayName='Output Features',
+            name='out_features',
+            datatype='GPFeatureLayer',
+            parameterType='Required',
+            direction='Output'
+        )
+        parameters = [in_features, out_features]
+        return parameters
+
+    def isLicensed(self):
+        """Set whether tool is licensed to execute."""
+        return True
+
+    def updateParameters(self, parameters):
+        """Modify the values and properties of parameters before internal
+        validation is performed.  This method is called whenever a parameter
+        has been changed."""
+        return
+
+    def updateMessages(self, parameters):
+        """Modify the messages created by internal validation for each tool
+        parameter.  This method is called after internal validation."""
+        return
+
+    def execute(self, parameters, messages):
+        in_features = parameters[0].valueAsText
+        out_features = parameters[1].valueAsText
+
+        conversion.line_2_polygon(in_features, out_features)
--- conversion.py
+++ conversion.py
@@ -17,11 +17,13 @@ def polygon_2_line(input_fc, output_fc):
 
     input_fc = arcpy.MultipartToSinglepart_management(input_fc)
     shapefieldname = arcpy.Describe(input_fc).ShapeFieldName
+
     # Cursor to create polylines
     cur = arcpy.da.InsertCursor(mem_fc, ['SHAPE@'])
 
     polycursor = arcpy.SearchCursor(input_fc)
     array = arcpy.Array()
+
     # Iterate over features in polygon FC and save vertices to array
     for feat in polycursor:
         polygon = feat.getValue(shapefieldname)
@@ -30,4 +32,54 @@ def polygon_2_line(input_fc, output_fc):
                 array.add(arcpy.Point(vert.X, vert.Y))
             cur.insertRow([arcpy.Polyline(array)])
             array.removeAll()
+
+    arcpy.CopyFeatures_management(mem_fc, output_fc)
+
+
+def line_2_polygon(input_fc, output_fc):
+    """
+    Converts a polyline FC into a polygon FC.
+
+    Extracts the vertices of each polyline and attaches the first vertex to the last. In the future, the goal is to make
+    the process work in a similar way to ArcGIS' process. That process only makes polygons out of lines that make an
+    enclosed area. Shown here:
+    http://desktop.arcgis.com/en/desktop/latest/tools/data-management-toolbox/GUID-601BAA73-E5EE-4275-AA89-68190423C8D2-web.png
+
+    Known issues:
+        This tool rounds every vertex to 4 decimal places. This isn't desirable for meters and feet coordinate systems,
+        but is a very bad thing indeed for coordinate systems such as WGS 84.
+        Not sure why it's doing this. I've read that ArcGIS rounds geometry without a defined coordinate system, but the
+        temporary FC, which is created on the first line, uses the input FC coordinate system. Furthermore, the
+        'polygon_2_line' function does not have the same problem, though it uses almost exactly the same logic. I've
+        narrowed down the issue as occurring when the InsertCursor writes the array using 'insertRow.' This may be an
+        ArcGIS bug. I'll work on a workaround soon.
+    :param input_fc:
+    :param output_fc:
+    :return:
+    """
+
+    # Create a FC that lives in memory, using the input FC spatial reference
+    mem_fc = arcpy.CreateFeatureclass_management('in_memory', 'mem_fc', 'POLYGON', None, None, None,
+                                                 arcpy.Describe(input_fc).spatialReference)
+    input_fc = arcpy.MultipartToSinglepart_management(input_fc)
+    shapefieldname = arcpy.Describe(input_fc).ShapeFieldName
+
+    # Cursor to create polylines
+    cur = arcpy.da.InsertCursor(mem_fc, ['SHAPE@'])
+
+    linecursor = arcpy.SearchCursor(input_fc)
+    array = arcpy.Array()
+
+    # Iterate over features in polyline FC and save vertices to array
+    for feat in linecursor:
+        line = feat.getValue(shapefieldname)
+        for vertices in line:
+            initial_x = vertices[0].X
+            initial_y = vertices[0].Y
+            for vert in vertices:
+                array.add(arcpy.Point(vert.X, vert.Y))
+            array.add(arcpy.Point(initial_x, initial_y))
+            cur.insertRow([arcpy.Polygon(array)])#
+            array.removeAll()
+
     arcpy.CopyFeatures_management(mem_fc, output_fc)
